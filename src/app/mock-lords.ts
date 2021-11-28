4642ca6dbcab031ea9ec2ff23a2f6eee34d72a36
abcdavid3 <david.evans0306@gmail.com>
2021-11-28 01:01:31 +0000

    Update mock-lords.ts
    Updating the names

--- src/app/mock-lords.ts
+++ src/app/mock-lords.ts
@@ -3,11 +3,11 @@ import { Lordtype } from "./lordtype";
 export const LORDS: Lordtype[] = [
     {id: 1, name: "Lord Vasraan", bio: "Vas", quote: "", imgsrc: "/assets/images/Vasraan.jpg"},
     {id: 2, name: "Lord Executor Val'haar", bio: "Val", quote: "They are a plague that will be cleansed by the might of the Empire", imgsrc: "/assets/images/Val2.png"},
-    {id: 3, name: "Lord Arcanus Yvass", bio: "Arc", quote: "Pathetic", imgsrc: "/assets/images/Yvass.jpg"},
+    {id: 3, name: "Lord Arcanus Y'vass", bio: "Arc", quote: "Pathetic", imgsrc: "/assets/images/Yvass.jpg"},
     {id: 4, name: "Lord Shadowhand Dauphine", bio: "Shad", quote: "Quote", imgsrc: "/assets/images/Dauphine.jpg"},
     {id: 5, name: "Lord Overseer Itharias", bio: "Over", quote: "Quote", imgsrc: "/assets/images/Itharias.png"},
     {id: 6, name: "Lord Rhozan", bio: "Rho", quote: "My comment on the rebellions? As far as I'm concerned, it isn't rebellion, it's treason. The citizens of these various planets have accepted the Empire. It is a minority who see fit to take hostages, destroy necessary infrastructure for their fellow citizens and landmarks in an attempt to secure any sort of meaningful purpose in their lives. These are disorganised criminals, riotous and uncouth", imgsrc: "/assets/images/Rhozan.jpg"},
-    {id: 7, name: "Lord Naressa", bio: "Nar", quote: "The rebellion is, quite plainly, a desperate action by desperate people. Something easily quelled and swept aside if not mis-managed. While it might be considered a noble act by some, fundamentally it is a tribal and chaotic course of action to take; while now full of promise, tenacity and fury it will ultimately achieve nothing", imgsrc: "/assets/images/Naressa.jpg"},
+    {id: 7, name: "Lord Nar'ressa", bio: "Nar", quote: "The rebellion is, quite plainly, a desperate action by desperate people. Something easily quelled and swept aside if not mis-managed. While it might be considered a noble act by some, fundamentally it is a tribal and chaotic course of action to take; while now full of promise, tenacity and fury it will ultimately achieve nothing", imgsrc: "/assets/images/Naressa.jpg"},
     {id: 8, name: "Lord Xachena", bio: "Xa", quote: "Quote", imgsrc: "/assets/images/xachena.png"},
     {id: 9, name: "Lord Maldrec", bio: "Mal", quote: "Quote", imgsrc: "/assets/images/Maldrec.jpg"},
     {id: 10, name: "Lord Esmee Koln", bio: "Esm", quote: "Quote", imgsrc: "/assets/images/Esmerelly.png"},
